b40ac1bec82d921148c282b7fe3292124ec78117
praveen.jeeru <praveenjeeru68@gmail.com>
2026-01-29 21:11:59 +0530

    commit

--- public/lib/stripe.ts
+++ public/lib/stripe.ts
@@ -1,5 +1,6 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  'pk_test_51Sojkk8Cp5TyQWFwFqRc4rBvuXuOebUKiaVqtvS1G54m61EMoejEUmgYoUXmGJU5pv0nzzePoWhQ8bX25726mP0P00AoMASDyi'
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
+
